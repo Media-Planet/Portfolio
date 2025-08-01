a059ac9ba572ffda1bbb7b09a6875021e41c0fc6
Peter <peaker122@gmail.com>
2025-08-01 11:46:21 +0300

    Digital Marketing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import WebDev from "./pages/services/webDevelopment/webDev";
 import BoardG from "./pages/services/boardGames/borderG";
+import DigitalMarketing from "./pages/services/digitalMarketing/digitalMarketing";
 
 function App() {
 
   return (
     <>
+      <DigitalMarketing />
       <BoardG />
       <WebDev />
     </>

0bf445787e83c0c800637eea7c23f660f918ecfb
Peter <peaker122@gmail.com>
2025-09-29 20:18:30 +0300

    fixed some bugs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,7 @@ import Layout from "./layout/layout";
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <Layout />
-=======
-      <Navbar />
-      {/* <WebDev /> */}
-      {/* <BoardG /> */}
-      {/* <DigitalMarketing /> */}
-      {/* <Home /> */}
-      <About />
-      <Footer />
->>>>>>> About_me_Media_Planet_1
     </>
   );
 }
